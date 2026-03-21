8b38fc966e97e205bbbcf32bac3f0d6c1044de4b
Gian <gianluca.cor@gmail.com>
2026-03-21 16:26:19 +0100

    remove old code

--- src/lib.h
+++ src/lib.h
@@ -1,7 +1,4 @@
 #include "FastLED.h"
 
-void lightningFrame(CRGB leds[], int from, int till, int widthFront, int widthBack, CRGB color, float framePercent);
 void clearLEDsInvisible(CRGB leds[]);
-void addToFirst(CRGB leds1[], CRGB leds2[]);
-float frameModifier(float points[], int size, float percentTime);
 void setBulk(CRGB leds[], int from, int to, CRGB color);
